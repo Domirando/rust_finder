354a649a9b6a58d2668be2b6606d5ca9d41ac10e
Domirando <vohidjonovnamaftuna@gmail.com>
2025-06-22 02:47:40 +0500

    changed

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { }, src ? ./. }:
-
-let theSource = src; in
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "rust_finder";
-  version = "0.1.0";
-  src = pkgs.lib.cleanSource "${theSource}";
-  cargoLock.lockFile = "${theSource}/Cargo.lock";
-}
