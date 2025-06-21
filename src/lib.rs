5a3bcfd0e4e4dac5b73614259dce484273453644
Domirando <vohidjonovnamaftuna@gmail.com>
2025-06-21 22:11:15 +0500

    changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,24 @@
-use std::fs;
+use std::fs::read_to_string;
 use std::error::Error;
 use std::env;
 
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-	let contents = fs::read_to_string(config.filename)?;
-	println!("with text:\n{}", contents);
-	let results = if config.case_sensitive {
-	    search(&config.query, &contents)
-	} else {
-	    search_case_insensitive(&config.query, &contents)
-	};   
-	// for line in results {
-	//     println!("{}", line);
+    // println!("{}", fs::read_to_string(config.filename));
+	// let contents = fs::read_to_string(config.filename)?;
+	for line in read_to_string(config.filename).unwrap().lines() {
+	    println!("{}", line.to_string());
+        // result.push(line.to_string())
+    }
+	// println!("with text:\n{}", result);
+	// let results = if config.case_sensitive {
+	//     search(&config.query, &contents)
+	// } else {
+	//     search_case_insensitive(&config.query, &contents)
+	// };   
+	// for line in search(&config.query, &contents) {
+	//     println!("so here is the finding - {}", line)
 	// }
-	for line in search(&config.query, &contents) {
-	    println!("{}", line)
-	}
 	Ok(())
 }
 
@@ -27,11 +30,11 @@ pub struct Config {
 
 impl Config {
 	pub fn new(args: &[String]) -> Result<Config, &str> {
-		if args.len() < 3 {
+	    if args.len() < 2 {
 			return Err("not enough arguments"); 
 		}               
-		let query = args[1].clone();
-		let filename = args[2].clone();
+		let query = args[0].clone();
+		let filename = args[1].clone();
 		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 		Ok( Config { query, filename, case_sensitive } )
 	}
@@ -59,30 +62,4 @@ pub fn search_case_insensitive<'a>(
         }
     }
     results
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn case_sensitive(){
-        let query = "duct";
-        //let filename = "peom.txt";
-        let contents = "\
-            Rust:
-            safe, Duct, Productive";
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        )
-    }
-    
-    #[test]
-    fn case_insensitive() {
-        let query = "";
-        
-        assert_eq!(
-            vec![]
-        )
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 // extern crate cliRustApp;
 use std::env;
 use std::process;
-use cliRustApp::Config;
+use rust_finder::Config;
+use std::io;
 
 fn main() {
-    // let a: String = env::args().nth(1).expect("cuz you damn didnt work");
-    let args: Vec<String> = env::args().collect();
+    println!("Please enter a word/phrase you are looking for:"); 
+    let mut search_input = String::new();
+    io::stdin().read_line(&mut search_input).expect("failed to read line");
+    
+    println!("Please provide a file name:"); 
+    let mut file_input = String::new();
+    io::stdin().read_line(&mut file_input).expect("failed to read line");
+    
+    println!("Do you want me to be case sensitivie? (yes/no"); 
+    let mut sensitivity_input = String::new();
+    io::stdin().read_line(&mut sensitivity_input).expect("failed to read line");
+    
+    let args: Vec<String> = vec![search_input, file_input, sensitivity_input];
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing argument: {}", err);
         process::exit(1);
     });
-    println!("searching for a sentence containing - '{}'", config.query);
-    println!("in file {}", config.filename);
-    if let Err(e) = cliRustApp::run(config) {
+    
+    if let Err(e) = rust_finder::run(config) {
         println!("application error: {}", e);
         process::exit(1);
     } 
