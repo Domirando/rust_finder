0760115590dfd5ab1f542f90fd4ffe881c80e33f
Domirando <vohidjonovnamaftuna@gmail.com>
2025-06-23 16:20:52 +0500

    changed

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::io;
 mod func;
 
 fn main(){
+    // let file = File::open("poem.txt");
+    // const POEM: &str = include_str!("poem.txt");
     println!("Please enter a word/phrase you are looking for:"); 
     let mut search_input = String::new();
     io::stdin().read_line(&mut search_input).expect("failed to read line");
@@ -19,7 +21,7 @@ fn main(){
     let mut sensitivity_input = String::new();
     io::stdin().read_line(&mut sensitivity_input).expect("failed to read line");
     
-    let args: Vec<String> = vec![search_input, file_input, sensitivity_input];
+    let args: Vec<String> = vec![search_input, "poem.txt".to_string(), sensitivity_input];
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing argument: {}", err);
         process::exit(1);
